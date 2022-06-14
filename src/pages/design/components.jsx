216dc47494672bf645854473a9ad0de5dbb9166a
jwseo-rbrain <jwseo@rbrain.co.kr>
2022-06-15 00:14:47 +0900

    [TASK] Mui 연동 / 컴포넌트 페이지 생성🧬

---
+++ src/pages/design/components.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Typography } from "@mui/material";
+const Components = (props) => {
+  return (
+    <React.Fragment>
+      <Typography>Documentation</Typography>
+    </React.Fragment>
+  );
+};
+
+export default Components;
